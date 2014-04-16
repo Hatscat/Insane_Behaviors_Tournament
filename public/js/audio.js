cbd9a03586583c2a2297121d776ee11222b1f965
solfen <solfen@hotmail.com>
2014-04-16 13:12:17 +0200

    fallback audio howler

--- public/js/audio.js
+++ public/js/audio.js
@@ -3,22 +3,21 @@ var Sound = function(ctx, url, volume, autoPlay, loop)
 	this.volume = volume || 1;
 	this.autoPlay = autoPlay || false;
 	this.loop = loop || false
+	this.ctx = ctx
 	
-	if(ctx === "useHowler")
+	if(this.ctx === "useHowler")
 	{
-		var sound = new Howl({  urls: [url],  
-			autoplay: this.autoPlay,  loop: this.loop,  volume: this.volume,  
-			});
-		this.play = sound.play;
-		this.stop = sound.stop;
+		this.sound = new Howl({  urls: [url + ".mp3", url + ".ogg"],  
+			autoplay: this.autoPlay,  loop: this.loop,  volume: this.volume, buffer: true,  
+		});
+
 	}
 
 	else
 	{
-		this.ctx = ctx
 		this.soundBuffer = null;
 		var request = new XMLHttpRequest();
-		request.open('GET', url, true);
+		request.open('GET', url + "ogg", true);
 		request.responseType = 'arraybuffer';
 		// Create a gain node.
 		this.gainNode = ctx.createGain();
@@ -45,22 +44,38 @@ var Sound = function(ctx, url, volume, autoPlay, loop)
 
 Sound.prototype.play = function (delay) 
 {
-	delay = delay || 0;
+	if(this.ctx === "useHowler")
+	{
+		this.sound.play();
+	}
+	else
+	{
+		delay = delay || 0;
 
-	this.source = this.ctx.createBufferSource(); 				// creates a sound source
-	this.source.loop = this.loop;								//sets the loop
-	this.source.buffer = this.soundBuffer;                   	// tell the source which sound to play
-	this.source.connect(this.gainNode);      					// connect the source to the node (serve to manipulate the sound)
-	this.gainNode.connect(this.ctx.destination) 				// connect the node to the destination (speaker)
-	this.gainNode.gain.value = this.volume;
+		this.source = this.ctx.createBufferSource(); 				// creates a sound source
+		this.source.loop = this.loop;								//sets the loop
+		this.source.buffer = this.soundBuffer;                   	// tell the source which sound to play
+		this.source.connect(this.gainNode);      					// connect the source to the node (serve to manipulate the sound)
+		this.gainNode.connect(this.ctx.destination) 				// connect the node to the destination (speaker)
+		this.gainNode.gain.value = this.volume;
 
-	this.source.start(delay);                           // play the source after the given delay
+		this.source.start(delay);                           // play the source after the given delay
+		
+	}
 }
 
 Sound.prototype.stop = function (delay) 
 {
-	delay = delay || 0;
-	this.source.stop(delay);                           // play the source after the given delay
+	if(this.ctx === "useHowler")
+	{
+		this.sound.stop()
+	}
+	else
+	{
+		delay = delay || 0;
+		this.source.stop(delay);                           // play the source after the given delay
+		
+	}
 }
 
 function init_sound_context(p_config) 
