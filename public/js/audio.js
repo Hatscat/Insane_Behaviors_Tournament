3d685f9f9c643cbfbb1a42e3f9c5bbfe80098b27
solfen <solfen@hotmail.com>
2014-04-16 12:17:19 +0200

    Fix new player

--- public/js/audio.js
+++ public/js/audio.js
@@ -1,41 +1,53 @@
 var Sound = function(ctx, url, volume, autoPlay, loop)
 {
+	if(ctx === "useHowler")
+	{
+
+	}
+	else
+	{
+		
+	}
 	this.ctx = ctx
-	this.volume = volume
+	this.volume = volume || 1;
+	this.autoPlay = autoPlay || false;
+	this.loop = loop || false
 	this.soundBuffer = null;
 	var request = new XMLHttpRequest();
 	request.open('GET', url, true);
 	request.responseType = 'arraybuffer';
 	// Create a gain node.
-	this.gainNode = context.createGain();
+	this.gainNode = ctx.createGain();
 
 	// Decode asynchronously
-	this = that;
+	var that = this;
 	request.onload = function() 
 	{
-	  this.ctx.decodeAudioData(request.response, function(buffer) 
-	  {
-	    that.soundBuffer = buffer;
-		that.source = that.ctx.createBufferSource(); 				// creates a sound source
-		that.source.loop = that.loop || false 						//sets the loop
-		that.source.buffer = that.soundBuffer;                   	// tell the source which sound to play
-		that.source.connect(gainNode);      						// connect the source to the node (serve to manipulate the sound)
-		that.gainNode.connect(that.ctx.destination) 				// connect the node to the destination (speaker)
-		that.gainNode.gain.value = that.volume;
+		var thut = that;
+		that.ctx.decodeAudioData(request.response, function(buffer) 
+		{
+		that.soundBuffer = buffer;
+		
 		if(that.autoPlay)
 			that.play();
-	  }, onError);
+		});
 	}
 	request.send();
 
-	if(autoPlay)
-		this.play();
 
 }
 
 Sound.prototype.play = function (delay) 
 {
 	delay = delay || 0;
+
+	this.source = this.ctx.createBufferSource(); 				// creates a sound source
+	this.source.loop = this.loop;								//sets the loop
+	this.source.buffer = this.soundBuffer;                   	// tell the source which sound to play
+	this.source.connect(this.gainNode);      					// connect the source to the node (serve to manipulate the sound)
+	this.gainNode.connect(this.ctx.destination) 				// connect the node to the destination (speaker)
+	this.gainNode.gain.value = this.volume;
+
 	this.source.start(delay);                           // play the source after the given delay
 }
 
@@ -47,7 +59,16 @@ Sound.prototype.stop = function (delay)
 
 function init_sound_context(p_config) 
 {
+  try 
+  {
+    // Fix up for prefixing
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    var context = new AudioContext();
+    return context
+  }
 
-   var context = new AudioContext();
-
+  catch(e) 
+  {
+  	return "useHowler";
+  }
 }
